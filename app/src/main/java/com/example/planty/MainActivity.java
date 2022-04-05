7ef3a4c22f44c6b54b9760bbeb2e822828c472b0
eszter nagy <eszteranagy@gmail.com>
2022-04-05 17:40:44 +0200

    elso issue

--- app/src/main/java/com/example/planty/MainActivity.java
+++ app/src/main/java/com/example/planty/MainActivity.java
@@ -3,12 +3,33 @@ package com.example.planty;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String LOG_TAG = MainActivity.class.getName();
+
+    EditText userNameET;
+    EditText passwordET;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        userNameET = findViewById(R.id.editTextUserName);
+        passwordET = findViewById(R.id.editTextPassword);
+    }
+
+    public void login(View view) {
+        String userName = userNameET.getText().toString();
+        String password = passwordET.getText().toString();
+
+
+
+        Log.i(LOG_TAG, "Bejelentkezett: "+ userName + ", jelszo: " + password);
+
     }
 }
